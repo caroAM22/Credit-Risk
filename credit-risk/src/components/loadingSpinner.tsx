63f361a5159005aca99eee9cfcddb9791c8874d7
caroAM22 <caalvarezmu@unal.edu.co>
2025-01-21 18:52:11 -0500

    Fix: message of loading spinner

--- credit-risk/src/components/loadingSpinner.tsx
+++ credit-risk/src/components/loadingSpinner.tsx
@@ -1,7 +1,8 @@
 const LoadingSpinner = () => {
     return (
-      <div className="flex justify-center items-center my-8">
+      <div className="flex flex-col justify-center items-center my-8">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-500"></div>
+        <p className="mt-4 text-sm text-gray-600">Esto puede tardar unos minutos...</p>
       </div>
     )
   }
