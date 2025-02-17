43040bfd6cc228876afc08e5ae14871ad58ef576
caroAM22 <caalvarezmu@unal.edu.co>
2025-02-16 20:58:25 -0500

    Fix: correciones segunda entrega

--- credit-risk/src/schemas/loanInfoSchema.ts
+++ credit-risk/src/schemas/loanInfoSchema.ts
@@ -2,88 +2,6 @@ import { z } from 'zod';
 import { errorMessages } from '../utils/errorMessages';
 
 export const loanInfoSchema = z.object({
-  loan_amnt: z.preprocess(
-    (val) => (isNaN(Number(val)) ? undefined : Number(val)),
-    z.number({
-      required_error: errorMessages.required,
-      invalid_type_error: errorMessages.invalidType,
-    })
-      .min(500, errorMessages.minValue(500))
-      .max(35000, errorMessages.maxValue(35000))
-  ),
-
-  funded_amnt: z.preprocess(
-    (val) => (isNaN(Number(val)) ? undefined : Number(val)),
-    z.number({
-      required_error: errorMessages.required,
-      invalid_type_error: errorMessages.invalidType,
-    }).min(500, errorMessages.minValue(500)).max(35000, errorMessages.maxValue(35000)),
-  ),
-
-  funded_amnt_inv: z.preprocess(
-    (val) => {
-      const numberValue = Number(val);
-      return isNaN(numberValue) ? null : numberValue; 
-    },
-    z.union([
-      z.number({
-        invalid_type_error: errorMessages.invalidType,
-      }).min(0, errorMessages.minValue(0)).max(35000, errorMessages.maxValue(35000)),
-      z.null(),
-    ])
-  )
-  .optional(),
-
-  term: z.preprocess(
-    (val) => (isNaN(Number(val)) ? undefined : Number(val)),
-    z.number({
-      required_error: errorMessages.required,
-      invalid_type_error: errorMessages.invalidType,
-    }).min(36, errorMessages.minValue(36)).max(60, errorMessages.maxValue(60)),
-  ),
-
-  int_rate: z.preprocess(
-    (val) => {
-      const numberValue = Number(val);
-      return isNaN(numberValue) ? null : numberValue; 
-    },
-    z.union([
-      z.number({
-        invalid_type_error: errorMessages.invalidType,
-      }).min(5.32, errorMessages.minValue(5.32)).max(28.99, errorMessages.maxValue(28.99)),
-      z.null(),
-    ])
-  )
-  .optional(),
-
-  installment: z.preprocess(
-    (val) => (isNaN(Number(val)) ? undefined : Number(val)),
-    z.number({
-      required_error: errorMessages.required,
-      invalid_type_error: errorMessages.invalidType,
-    }).min(15.67, errorMessages.minValue(15.67)).max(1445.46, errorMessages.maxValue(1445.46)),
-  ),
-
-  grade: z.preprocess(
-    (val) => (val === "" ? undefined : val),
-    z.enum(['b', 'c', 'e', 'f', 'g'], {
-      required_error: errorMessages.required,
-    })
-  ),
-
-  sub_grade: z.union([
-    z.enum(['b', 'c', 'e', 'f', 'g']),
-    z.null(),
-    z.literal("").transform(() => null),
-  ]).optional(),
-
-  desc: z.preprocess(
-    (val) => (val === "" ? undefined : val),
-    z.string({
-      required_error: errorMessages.required,
-      invalid_type_error: errorMessages.invalidType,
-    }).min(1, errorMessages.minLength(1)).max(512, errorMessages.maxLength(512))),
-
   purpose: z.preprocess(
     (val) => (val === "" ? undefined : val),
     z.enum([
@@ -104,12 +22,4 @@ export const loanInfoSchema = z.object({
       required_error: errorMessages.required,
     })
   ),
-
-  title: z.preprocess(
-    (val) => (val === "" ? undefined : val),
-    z.string({
-      required_error: errorMessages.required,
-      invalid_type_error: errorMessages.invalidType,
-    }).min(1, errorMessages.minLength(1)).max(124, errorMessages.maxLength(124))),
-
 });
